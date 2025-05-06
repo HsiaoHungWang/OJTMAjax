0a7bb34f5778335e840974ce72397140ffc35a9a
HsiaoHungWang <wang@ispan.com.tw>
2025-05-06 20:19:04 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OJTMAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
