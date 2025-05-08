027ba40f33cf77c88f77e9fca8f24fc6a67fa1f5
HsiaoHungWang <wang@ispan.com.tw>
2025-05-08 21:29:38 +0800

    第一支Ajax程式

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OJTMAjax.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Ajax 好!!","text/plain", System.Text.Encoding.UTF8);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace OJTMAjax.Controllers
             return View();
         }
 
+        public IActionResult FirstAjax()
+        {     
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
