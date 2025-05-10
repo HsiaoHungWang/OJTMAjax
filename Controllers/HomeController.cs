752b85fc45705daef84306b43439d5339b2b9815
HsiaoHungWang <wang@ispan.com.tw>
2025-05-10 15:16:35 +0800

    連動式的Select標籤寫法

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -51,6 +51,10 @@ namespace OJTMAjax.Controllers
             return View();
         }
 
+        public IActionResult ShowImage() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
